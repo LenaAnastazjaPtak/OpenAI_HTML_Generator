a1279f11d2ac9c57bb190d7f19fe75cac91ce6ed
LenaAnastazjaPtak <lena.anastazja.ptak@gmail.com>
2024-11-13 20:45:29 +0100

    generate by prompt func, generate html prompt

--- main.py
+++ main.py
@@ -5,18 +5,8 @@ from dotenv import load_dotenv
 def read_article(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
-
-def generate_html_from_article(article_text):
-    prompt = (
-        "Przygotuj kod HTML dla artykułu zgodnie z następującymi wytycznymi:\n"
-        "1. Użyj odpowiednich tagów HTML do strukturyzacji treści.\n"
-        "2. W miejscach, gdzie warto umieścić grafikę, użyj tagu <img> z atrybutem src=\"image_placeholder.jpg\".\n"
-        "3. Dodaj atrybut alt do każdego obrazka z dokładnym promptem do wygenerowania grafiki.\n"
-        "4. Umieść podpisy pod grafikami za pomocą odpowiedniego tagu HTML.\n"
-        "5. Wygeneruj wyłącznie zawartość, którą można umieścić między <body> i </body>, bez użycia znaczników <html>, <head> czy <body>.\n\n"
-        "Artykuł:\n" + article_text
-    )
-
+    
+def generate(prompt):
     load_dotenv()
     client = OpenAI(
         api_key = os.getenv("OPENAI_API_KEY"),
@@ -37,14 +27,29 @@ def generate_html_from_article(article_text):
 def save_html(file_path, html_content):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(html_content)
+    print("Wygenerowano plik: ", file_path)
 
 def main():
     article_text = read_article("article.txt")
 
-    html_content = generate_html_from_article(article_text)
+    html_template_prompt = ("Wygeneruj szablon HTML z pustym <body>. Nie wyświetlaj tego jako kod HTML tylko jako tekst.")
 
+    html_content_prompt = (
+        "Przygotuj kod dla artykułu zgodnie z następującymi wytycznymi:\n"
+        "1. Użyj odpowiednich tagów HTML do strukturyzacji treści.\n"
+        "2. W miejscach, gdzie warto umieścić grafikę, użyj tagu <img> z atrybutem src=\"image_placeholder.jpg\".\n"
+        "3. Dodaj atrybut alt do każdego obrazka z dokładnym promptem do wygenerowania grafiki.\n"
+        "4. Umieść podpisy pod grafikami za pomocą odpowiedniego tagu HTML.\n"
+        "5. Wygeneruj wyłącznie zawartość, którą można umieścić między <body> i </body>, bez użycia znaczników <html>, <head> czy <body>.\n\n"
+        "6. Nie wyświetlaj tego jako kod HTML tylko jako tekst.\n"
+        "Artykuł:\n" + article_text
+    )
+
+    html_template = generate(html_template_prompt)
+    save_html("szablon.html", html_template)
+
+    html_content = generate(html_content_prompt)
     save_html("artykul.html", html_content)
-    print("Wygenerowano plik artykul.html")
 
 if __name__ == "__main__":
     main()
